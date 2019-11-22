8f3622e21f59646faee0e1fd49abdb17d8452b1a
Sebastian <monika.lukowska@gmail.com>
2019-11-22 20:49:17 +0100

    v2.0

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
+//
+const cityId = 2643743;
+
 function getWeather(cityID) {
     const key = '{5480f732e4b00a16aa00aaf7e311ee62}';
-    fetch(`http://api.openweathermap.org/data/2.5/forecast?id=${cityID}&APPID=${key}`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?id=${cityId}&appid=${key}`)
         .then(function (resp) {
             return resp.json()
         })
@@ -18,5 +21,18 @@ function displayWeather(data) {
     document.getElementById('weather').innerHTML = data.weather[0].description;
     document.getElementById('wind').innerHTML = `Wiatr: ${data.wind.speed}km/h`;
     document.getElementById('humidity').innerHTML = `Wilgotność: ${data.main.humidity}`;
+    document.getElementById('main-icon').src.innerHTML = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+
+    if (data.weather[0].main === 'Rain') {
+        document.body.className = 'rainy';
+    } else if (data.weather[0].main === 'Clouds') {
+        document.body.className = 'cloudy';
+    } else if (data.weather[0].main === 'Snow') {
+        document.body.className = 'snowy';
+    } else if (data.weather[0].main === 'Thunderstorm') {
+        document.body.className = 'stormy';
+    } else if (data.weather[0].main === 'Clear') {
+        document.body.className = 'suny';
+    }
 }
-getWeather(4175117);
+getWeather(2643743);
