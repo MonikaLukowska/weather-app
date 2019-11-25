b5d39a0d38be6ddc2383910634ca27dc9413a218
Sebastian <monika.lukowska@gmail.com>
2019-11-25 21:39:47 +0100

    new backgrounds, carousel added

--- main.js
+++ main.js
@@ -1,38 +1,54 @@
-//
-const cityId = 2643743;
+let cityId = 420006353;
 
-function getWeather(cityID) {
-    const key = '{5480f732e4b00a16aa00aaf7e311ee62}';
+//fetch api
+function getWeather(cityId) {
+    const key = '5480f732e4b00a16aa00aaf7e311ee62';
     fetch(`https://api.openweathermap.org/data/2.5/weather?id=${cityId}&appid=${key}`)
-        .then(function (resp) {
-            return resp.json()
-        })
-        .then(function (data) {
+        .then(response => response.json())
+        .then((data) => {
             displayWeather(data)
+            //put data from api
+            function displayWeather(data) {
+                const celcius = Math.round(parseFloat(data.main.temp) - 273.15);
+                const icon = data.weather[0].icon;
+
+                document.getElementById('city-name').innerHTML = data.name;
+                document.getElementById('temperature').innerHTML = celcius + '&deg;';
+                document.getElementById('weather').innerHTML = data.weather[0].description;
+                document.getElementById('wind').innerHTML = `Wiatr: ${data.wind.speed}km/h`;
+                document.getElementById('humidity').innerHTML = `Wilgotność: ${data.main.humidity}%`;
+                document.getElementById('main-icon').src = `http://openweathermap.org/img/wn/${icon}@2x.png`
+
+
+                //change background
+                const main = data.weather[0].main;
+
+
+                if (main === 'Rain') {
+                    document.body.className = 'rainy';
+                } else if (main === 'Clouds') {
+                    document.body.className = 'cloudy';
+                } else if (main === 'Snow') {
+                    document.body.className = 'snowy';
+                } else if (main === 'Thunderstorm') {
+                    document.body.className = 'stormy';
+                } else {
+                    document.body.className = 'sunny';
+                }
+            }
+
         })
-        .catch(function () {});
 }
+getWeather(cityId);
 
-function displayWeather(data) {
-    const celcius = Math.round(parseFloat(data.main.temp) - 273.15);
 
-    document.getElementById('city-name').innerHTML = data.name;
-    document.getElementById('temperature').innerHTML = celcius + '&deg;';
-    document.getElementById('weather').innerHTML = data.weather[0].description;
-    document.getElementById('wind').innerHTML = `Wiatr: ${data.wind.speed}km/h`;
-    document.getElementById('humidity').innerHTML = `Wilgotność: ${data.main.humidity}`;
-    document.getElementById('main-icon').src.innerHTML = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+$('.slide-container').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    useCss: false,
+    autoplaySpeed: 2000,
+    nextArrow: $('.next'),
+    prevArrow: $('.prev'),
 
-    if (data.weather[0].main === 'Rain') {
-        document.body.className = 'rainy';
-    } else if (data.weather[0].main === 'Clouds') {
-        document.body.className = 'cloudy';
-    } else if (data.weather[0].main === 'Snow') {
-        document.body.className = 'snowy';
-    } else if (data.weather[0].main === 'Thunderstorm') {
-        document.body.className = 'stormy';
-    } else if (data.weather[0].main === 'Clear') {
-        document.body.className = 'suny';
-    }
-}
-getWeather(2643743);
+});
