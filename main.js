73633895250a874ddbd503a286a62bd0283e56e0
Łukasz Szulc <lukasz.szulc@op.pl>
2019-11-26 21:59:16 +0100

    Skrypt pogoda 5-dniowa

--- main.js
+++ main.js
@@ -39,15 +39,75 @@ function getWeather(cityId) {
 
         })
 }
+
+// function getForecast(cityId) {
+//     const key = '5480f732e4b00a16aa00aaf7e311ee62';
+//     fetch(`https://api.openweathermap.org/data/2.5/forecast?id=${cityId}&appid=${key}`)
+//         .then(response => response.json())
+//         .then((data) => {
+//             displayForecast(data)
+//             //put data from api
+//             function displayForecast(data) {
+//                 console.log(data);
+//                 for (let value of [0, 1, 2, 3, 4]) {
+//                     document.getElementById(`day${value+1}`).innerHTML = data.list[value*8].dt_txt;
+//                     document.getElementById(`icon${value+1}`).src = `http://openweathermap.org/img/wn/${data.list[value*8].weather[0].icon}@2x.png`
+//                     document.getElementById(`temp${value+1}`).innerHTML = Math.round(parseFloat(data.list[value*8].main.temp) - 273.15) + '&deg;';
+//                     document.getElementById(`wind${value+1}`).innerHTML = `Wiatr: ${data.list[value*8].wind.speed}km/h`;
+//                     console.log(value);
+//                 }
+//             }
+
+//         })
+// }
+
+
+// function getForecast(cityId) {
+
+//         document.getElementById(`day1`).innerHTML = "Poniedziałek";
+//         document.getElementById(`icon1`).src = 'img\amcharts_weather_icons_1.0.0\static\day.svg';
+//         document.getElementById(`temp1`).innerHTML = "20";
+//         document.getElementById(`wind1`).innerHTML = `Wiatr: 50km/h`;
+//         document.getElementById(`day5`).innerHTML = "Piątek";
+//         document.getElementById(`icon5`).src = 'img\amcharts_weather_icons_1.0.0\static\day.svg';
+//         document.getElementById(`temp5`).innerHTML = "20";
+//         document.getElementById(`wind5`).innerHTML = `Wiatr: 50km/h`;
+//     }
+                
+function getForecast(cityId) {
+    const key = '5480f732e4b00a16aa00aaf7e311ee62';
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?id=${cityId}&appid=${key}`)
+        .then(response => response.json())
+        .then((data) => {
+            displayForecast(data)
+            //put data from api
+            function displayForecast(data) {
+                console.log(data);
+                for (let value of [0, 1, 2, 3, 4]) {
+                    document.getElementById(`day${value+1}`).innerHTML = data.list[value*8].dt_txt;
+                    document.getElementById(`icon${value+1}`).src = `http://openweathermap.org/img/wn/${data.list[value*8].weather[0].icon}@2x.png`
+                    document.getElementById(`temp${value+1}`).innerHTML = Math.round(parseFloat(data.list[value*8].main.temp) - 273.15) + '&deg;';
+                    document.getElementById(`wind${value+1}`).innerHTML = `Wiatr: ${data.list[value*8].wind.speed}km/h`;
+                    console.log(value);
+                }
+            }
+
+        })
+}
+
+
+
 getWeather(cityId);
+getForecast(cityId);
+
 
 
 $('.slide-container').slick({
     slidesToShow: 3,
     slidesToScroll: 1,
-    autoplay: true,
+    autoplay: false,
     useCss: false,
-    autoplaySpeed: 2000,
+    autoplaySpeed: 3000,
     nextArrow: $('.next'),
     prevArrow: $('.prev'),
 
